f77f47a6ec5061f80aeef01fcc9029042eef7347
Alex Shamray <gibsonshamray@gmail.com>
2019-08-15 14:30:22 +0300

    Homework2 Exercise1 Homework2 Exercise2

--- src/main/java/com/playtika/javacourse/HomeWork2Ex1.java
+++ src/main/java/com/playtika/javacourse/HomeWork2Ex1.java
@@ -4,65 +4,87 @@ import java.util.Scanner;
 
 public class HomeWork2Ex1 {
 
+    static final int SIDE_ASTERISK = 4; // +4 additional asterisks acording to frame format "* <string> *"
+
     public static void main(String[] args) {
 
         Scanner console = new Scanner(System.in);
         console.useDelimiter("\n"); // set end line as Delimeter (to accept spaces in the entered string)
 
-        String str1 = "Курс: Java core"; // 1я строка
-        String str2 = "Студент"; // 2я строка
+                final String str1 = "Курс: Java core"; // 1я строка
+        final String str2 = "Студент"; // 2я строка
         // 3я строка - ФИО
         System.out.println("Введите ваше ФИО:");
         String str3 = console.next();
 
-        int strlength; // length of the max string
-        if (str1.length() > str2.length()) {
-            strlength = str1.length();
-        }
-        else {
-            strlength = str2.length();
-        }
+        int maxStringLength = getMaxStringLength(str1, str2, str3); // length of the max string
 
-        if (str3.length() > strlength) {
-            strlength = str3.length();
-        }
 
-        // output
-        asterisk(strlength);
-        title(str1, strlength);
-        title(str2, strlength);
-        title(str3, strlength);
-        asterisk(strlength);
+        asterisk(maxStringLength);
+        title(str1, maxStringLength);
+        title(str2, maxStringLength);
+        title(str3, maxStringLength);
+        asterisk(maxStringLength);
 
     }
 
-    // method for the frame of astrisks
-    public static void asterisk(int maxlength){
-        for (int i = 0; i < (maxlength +4); i++){
+    // method for the frame of asterisks
+    public static void asterisk(int maxLength){
+        for (int i = 0; i < (maxLength + SIDE_ASTERISK); i++){
             System.out.print("*");
         }
         System.out.println();
     }
 
     // method for title typing
-    public static void title(String str, int maxlength){
+    public static void title(String str, int maxLength){
         System.out.print("* ");
 
-        int x1 = (maxlength - str.length())/2;
+        //int x1 = (maxLength - str.length())/2;
+        int leftIndent = getLeftIndent(maxLength, str);
         // spaces on the left
-        for (int i = 0; i < x1; i++){
+        for (int i = 0; i < leftIndent; i++){
             System.out.print(" ");
         }
 
         System.out.print(str);
 
-        int x2 = (maxlength - str.length()) - x1;
+        int rightIndent = getRightIndent(maxLength, str);
         // spaces on the right
-        for (int i = 0; i < x2; i++){
+        for (int i = 0; i < rightIndent; i++){
             System.out.print(" ");
         }
 
         System.out.println(" *");
+    }
+
+    // Define amount of spaces on the left
+    public static int getLeftIndent(int maxLength, String str){
+        int x = (maxLength - str.length())/2;
+        return x;
+    }
+
+    // Define amount of spaces on the right
+    public static int getRightIndent(int maxLength, String str){
+        int x = (maxLength - str.length()) - getLeftIndent(maxLength, str);
+        return x;
+    }
+
+    public static int getMaxStringLength(String str1, String str2, String str3)
+    {
+        int maxStringLength;
+        if (str1.length() > str2.length()) {
+            maxStringLength = str1.length();
+        }
+        else {
+            maxStringLength = str2.length();
+        }
+
+        if (str3.length() > maxStringLength) {
+            maxStringLength = str3.length();
+
+        }
+        return maxStringLength;
     }
 
 }
