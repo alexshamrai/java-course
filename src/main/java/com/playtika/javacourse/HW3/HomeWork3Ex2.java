596fc78b444c00cc409f89dee7e1e8c21cbf6571
Alex Shamray <gibsonshamray@gmail.com>
2019-08-25 11:58:24 +0300

    Homework3

---
+++ src/main/java/com/playtika/javacourse/HW3/HomeWork3Ex2.java
@@ -0,0 +1,41 @@
+package com.playtika.javacourse.HW3;
+import java.util.Scanner;
+
+public class HomeWork3Ex2 {
+    public static void main(String[] args) {
+        String str = getConsoleInput();
+        String[] subStrings = str.split(" ");
+        System.out.println(getModifiedString(subStrings));
+    }
+
+    // method for getting a string from console
+    static String getConsoleInput(){
+        Scanner console = new Scanner(System.in);
+        console.useDelimiter("\n"); // set end line as Delimeter (to accept spaces in the entered string)
+
+        System.out.println("Введите строку:");
+        String input = console.next();
+        return input;
+    }
+
+    // get reversed single word using string builder
+    static String getReversedWord(String word){
+        StringBuilder stringBuilderWord = new StringBuilder(word);
+        stringBuilderWord = stringBuilderWord.reverse();
+        return stringBuilderWord.toString();
+    }
+
+    //get modified string with reversed words
+    static String getModifiedString(String[] subStrings){
+        StringBuilder concatString = new StringBuilder();
+
+        for (int i = 0; i < subStrings.length; i++){
+            concatString.append(getReversedWord(subStrings[i]));
+            if (i != subStrings.length-1){
+                concatString.append(" ");
+            }
+        }
+
+        return concatString.toString();
+    }
+}
