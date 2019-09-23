317a59976465d4966918d519840ee4583d384c52
Alex Shamray <gibsonshamray@gmail.com>
2019-09-23 19:34:06 +0300

    HW5 + exception handling

--- src/main/java/com/playtika/javacourse/HW5/Calculator.java
+++ src/main/java/com/playtika/javacourse/HW5/Calculator.java
@@ -5,11 +5,11 @@ package com.playtika.javacourse.HW5;
  */
 public class Calculator {
 
-    public double calculate(double val1, double val2, String operator) {
+    public double calculate(double val1, double val2, String operator) throws NullPointerException{
         BinaryOperation operation = getOperationFor(operator);
 
         if (operation == null) {
-            System.out.println("Неизвестный оператор " + operator);
+            System.out.print("Неизвестный оператор: " + operator + " - ");
             return Double.NaN;
         }
 
@@ -18,7 +18,7 @@ public class Calculator {
 
 
     //define operator via comparing with enum parameters (returns class of needed operation or null)
-    private BinaryOperation getOperationFor(String operator) {
+    private BinaryOperation getOperationFor(String operator) throws NullPointerException {
             for(OperatorsAvailable Available: OperatorsAvailable.values()){
                 if (operator.equals(Available.getOperatorString())) {
                     return new OperatorFactory().getOperator(Available);
@@ -30,9 +30,17 @@ public class Calculator {
     public static void main(String[] args) {
         Parser expression = new Parser();
         Calculator calculator = new Calculator();
-        expression.parse();
+        try {
+            expression.parse();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
+            System.out.println("Expression is incorrect. Exception: " + e.getMessage());
+        }
+        try {
+            System.out.println(calculator.calculate(expression.getLeftOperand(), expression.getRightOperand(), expression.getOperator()));
+        } catch (NullPointerException | IllegalArgumentException e) {
+            System.out.println("Operator or operand is incorrect. Error: " + e.getMessage());
+        }
 
-        System.out.println(calculator.calculate(expression.getLeftOperand(), expression.getRightOperand(), expression.getOperator()));
 
     }
 }
